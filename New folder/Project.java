4f40b9d4b76b52c41dc86ef2c6d02bf8c2176175
Hussein Bitambuka <76148389+HusseinBitambuka@users.noreply.github.com>
2023-04-12 23:39:33 -0400

    Delete New folder directory

--- New folder/Project.java
+++
@@ -1,6 +0,0 @@
-public interface Project {
-    public static void main(String [] args){
-        Student galila= new Student("Galila", "Black", 1010, "Ethiopia");
-        System.out.print(galila.name);
-    }
-}

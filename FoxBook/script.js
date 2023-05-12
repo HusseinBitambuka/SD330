d52caa43f3701e27b1a056d97a2ef06ede9b7c2e
Hussein Bitambuka <76148389+HusseinBitambuka@users.noreply.github.com>
2023-05-11 23:32:58 -0400

    foxBook

---
+++ FoxBook/script.js
@@ -0,0 +1,18 @@
+const classList = document.getElementById("class-list");
+
+fetch("books.json")
+  .then(response => response.json())
+  .then(data => {
+    data.books.forEach(book => {
+      const classItem = document.createElement("li");
+      const classLink = document.createElement("a");
+      classLink.textContent = book.class;
+      classLink.addEventListener("click", () => {
+        const bookInfo = document.createElement("p");
+        bookInfo.textContent = `Title: ${book.title} | Professor: ${book.professor}`;
+        classItem.appendChild(bookInfo);
+      });
+      classItem.appendChild(classLink);
+      classList.appendChild(classItem);
+    });
+  });
